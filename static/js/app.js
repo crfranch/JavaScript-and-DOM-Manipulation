aa7e4d4c2d33d55650d3ff8f7853a5e3dcdccb2f
Franch Fry <crfranchi93@gmail.com>
2019-05-02 14:28:24 -0700

    testing app.js

--- static/js/app.js
+++ static/js/app.js
@@ -19,9 +19,9 @@ data.forEach(function(UFOSighting) {
 
 // Listen for events and search through the date/time column to find rows that match user input
 // Select the submit button
-var button = d3.select("#filter-btn");
+var submitButton = d3.select("#filter-btn");
 
-button.on("click", function() {
+submitButton.on("click", function() {
 
 // Prevent the page from refreshing in order to execute the js function
   d3.event.preventDefault();
@@ -30,28 +30,27 @@ button.on("click", function() {
   var inputElement = d3.select("#filter-btn");
 
 // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+  var inputValue = inputElement.property("value");
 
+// Print input value and data to console
 console.log(inputValue);
 console.log(tableData);
 
 // Search through the date/time column
 var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
+// Print filtered data to console
 console.log(filteredData);
 
-// BONUS: Using multiple input tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
-// First, create an array with just the age values
-var ages = filteredData.map(person => person.age);
+// Finally, add the filtered UFO Sighting data to the `table-head` tag
 
-// // Finally, add the filtered UFO Sighting data to the `li` tag
 d3.select("ufo-table")
   .append("table-head").text(`Date: ${datetime}`)
-  .append("table-head").text(`City: ${City}`)
+  .append("table-head").text(`City: ${City}`);
   .append("table-head").text(`State: ${State}`)
   .append("table-head").text(`Country: ${Country}`)
   .append("table-head").text(`Shape: ${Shape}`)
   .append("table-head").text(`Duration: ${durationMinutes}`)
   .append("table-head").text(`Comments: ${comments}`)
-
-});
+})
+;
